e1f6fdb218245ebd111b5613ffe28162e8bf0b22
bkarki <bishwakarki2020@gmail.com>
2020-02-21 19:21:33 -0500

    new commit

--- alb.tf
+++ alb.tf
@@ -9,3 +9,18 @@ resource "aws_lb" "default" {
     Environment = "dev"
   }
 }
+resource "aws_lb_listener" "default" {
+  load_balancer_arn = "${aws_lb.default.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.default.arn}"
+  }
+}
+resource "aws_lb_target_group" "default" {
+  name     = "terraform-springboot-helloapp"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = "vpc-f8a48e82"
+}
